c5f17f273298bc7e90a2c7e7aa55cd8d87629cfa
jon <jonathanchesterfield@gmail.com>
2024-04-25 02:30:04 +0000

    conveniently update the specified branches without accidentally pulling in other branches as well

--- create_git_mirror.sh
+++ create_git_mirror.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Set up a directory as a git repo that reflects changes from one
-# git repo to another, e.g. to export a local one to github
-# Takes a non-empty list of branches and only copies those ones.
+# Arrange to mirror specific branches from an internal git repo to somewhere else
+# For example, export release branches to github without also posting various dev branches
+# Takes at least four arguments
+# Internal git location
+# External git location
+# What directory to set up as the staging ground
+# One or more branches to mirror
 
 set -e
 set -x
@@ -34,10 +38,22 @@ git -C "$WORKDIR" init --bare
 git -C "$WORKDIR" remote add internal "$INTERNAL"
 git -C "$WORKDIR" remote add external "$EXTERNAL"
 
-# Change fetch to apply to the local branches
-git -C "$WORKDIR" config remote.internal.fetch '+refs/heads/*:refs/heads/*'
+# Want git fetch --all to update the local branches and not pull additional branches from the remote
+# git fetch --all won't change the local branches, and then push --mirror has nothing to do
+
+# This unfortunately means it'll create a local branch for all remote ones, no deal
+# git -C "$WORKDIR" config remote.internal.fetch '+refs/heads/*:refs/heads/*'
+
+# Set up fetch -all to retrive exactly the specified branches
+git -C "$WORKDIR" config --unset-all remote.internal.fetch
+for BRANCH in $BRANCHES; do
+  git -C "$WORKDIR" config --add remote.internal.fetch '+refs/heads/'"$BRANCH"':refs/remotes/internal/'"$BRANCH"
+done
 
 git -C "$WORKDIR" fetch --all
+
+# Create local branches with the same names. These are the ones that push --mirror will
+# instantiate on the destination.
 for BRANCH in $BRANCHES; do
     git -C "$WORKDIR" branch "$BRANCH" internal/"$BRANCH"
 done
