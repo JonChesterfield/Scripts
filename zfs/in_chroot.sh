c401e9f948e60ddfe4bac3326127576e22c6d04f
jon <jonathanchesterfield@gmail.com>
2025-08-17 23:13:52 +0000

    right, need a different script for in the chroot

---
+++ zfs/in_chroot.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+set -e
+set -x
+set -o pipefail
+
+
+id=`id -u`
+if [ $id = "0" ]
+then
+    echo "Running as root"
+else
+    echo "Meant to run as root"
+    exit 1
+fi
+
+
+if [ "$#" -ne 0 ]; then
+    echo "Check arguments, expected none"
+    ls /dev/disk/by-id
+    exit 1
+fi
+
+echo "Disk = $DISK"
+
+mount --make-private --rbind /dev  /mnt/dev
+mount --make-private --rbind /proc /mnt/proc
+mount --make-private --rbind /sys  /mnt/sys
+chroot /mnt /usr/bin/env DISK=$DISK bash --login
+
+
+apt update
+apt install --yes console-setup locales
+
+echo "Want en_US.UTF-8"
+
+dpkg-reconfigure locales tzdata 
+
+
+apt install --yes dpkg-dev linux-headers-generic linux-image-generic
+apt install --yes zfs-initramfs
+echo REMAKE_INITRD=yes > /etc/dkms/zfs.conf
+
+# time
+apt install systemd-timesyncd
+
+apt install dosfstools
+
+mkdosfs -F 32 -s 1 -n EFI ${DISK}-part2
+mkdir /boot/efi
+echo /dev/disk/by-uuid/$(blkid -s UUID -o value ${DISK}-part2) \
+   /boot/efi vfat defaults 0 0 >> /etc/fstab
+mount /boot/efi
+apt install --yes grub-efi-amd64 shim-signed
+
+apt purge --yes os-prober
+
+passwd
+
+cat <<EOF > /etc/systemd/system/zfs-import-bpool.service
+[Unit]
+DefaultDependencies=no
+Before=zfs-import-scan.service
+Before=zfs-import-cache.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/sbin/zpool import -N -o cachefile=none bpool
+# Work-around to preserve zpool cache:
+ExecStartPre=-/bin/mv /etc/zfs/zpool.cache /etc/zfs/preboot_zpool.cache
+ExecStartPost=-/bin/mv /etc/zfs/preboot_zpool.cache /etc/zfs/zpool.cache
+
+[Install]
+WantedBy=zfs-import.target
+
+EOF
+
+systemctl enable zfs-import-bpool.service
+
+
+apt install --yes openssh-server
+
+# temporary
+sed -i sed 's$#PermitRootLogin prohibit-password$PermitRootLogin yes # todo, undo this...$g' /etc/ssh/sshd_config
+
+grub-probe /boot
+update-initramfs -c -k all
+
+
+sed -i 's$GRUB_CMDLINE_LINUX=""$GRUB_CMDLINE_LINUX="root=ZFS=rpool/ROOT/debian"$' /etc/default/grub
+
+sed -i 's$GRUB_CMDLINE_LINUX_DEFAULT="quiet"$GRUB_CMDLINE_LINUX_DEFAULT=""$' /etc/default/grub
+
+sed -i 's$#GRUB_TERMINAL=console$GRUB_TERMINAL=console$' /etc/default/grub
+
+update-grub
+
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi \
+     --bootloader-id=debian --recheck --no-floppy
+
+mkdir /etc/zfs/zfs-list.cache
+touch /etc/zfs/zfs-list.cache/bpool
+touch /etc/zfs/zfs-list.cache/rpool
+zed -F
+
+echo "Hopefully these aren't empty"
+cat /etc/zfs/zfs-list.cache/bpool
+cat /etc/zfs/zfs-list.cache/rpool
+
+
+
+ 
+sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/*
+
+zfs snapshot bpool/BOOT/debian@install
+zfs snapshot rpool/ROOT/debian@install
+
+exit
--- zfs/root_first.sh
+++ zfs/root_first.sh
@@ -137,100 +137,13 @@ deb http://deb.debian.org/debian trixie-updates main contrib non-free-firmware
 deb-src http://deb.debian.org/debian trixie-updates main contrib non-free-firmware
 EOF
 
+cp in_chroot.sh /mnt/tmp/in_chroot.sh
+
 mount --make-private --rbind /dev  /mnt/dev
 mount --make-private --rbind /proc /mnt/proc
 mount --make-private --rbind /sys  /mnt/sys
 chroot /mnt /usr/bin/env DISK=$DISK bash --login
 
-apt update
-apt install --yes console-setup locales
-
-echo "Want en_US.UTF-8"
-
-dpkg-reconfigure locales tzdata 
-
-
-apt install --yes dpkg-dev linux-headers-generic linux-image-generic
-apt install --yes zfs-initramfs
-echo REMAKE_INITRD=yes > /etc/dkms/zfs.conf
-
-# time
-apt install systemd-timesyncd
-
-apt install dosfstools
-
-mkdosfs -F 32 -s 1 -n EFI ${DISK}-part2
-mkdir /boot/efi
-echo /dev/disk/by-uuid/$(blkid -s UUID -o value ${DISK}-part2) \
-   /boot/efi vfat defaults 0 0 >> /etc/fstab
-mount /boot/efi
-apt install --yes grub-efi-amd64 shim-signed
-
-apt purge --yes os-prober
-
-passwd
-
-cat <<EOF > /etc/systemd/system/zfs-import-bpool.service
-[Unit]
-DefaultDependencies=no
-Before=zfs-import-scan.service
-Before=zfs-import-cache.service
-
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/sbin/zpool import -N -o cachefile=none bpool
-# Work-around to preserve zpool cache:
-ExecStartPre=-/bin/mv /etc/zfs/zpool.cache /etc/zfs/preboot_zpool.cache
-ExecStartPost=-/bin/mv /etc/zfs/preboot_zpool.cache /etc/zfs/zpool.cache
-
-[Install]
-WantedBy=zfs-import.target
-
-EOF
-
-systemctl enable zfs-import-bpool.service
-
-
-apt install --yes openssh-server
-
-# temporary
-sed -i sed 's$#PermitRootLogin prohibit-password$PermitRootLogin yes # todo, undo this...$g' /etc/ssh/sshd_config
-
-grub-probe /boot
-update-initramfs -c -k all
-
-
-sed -i 's$GRUB_CMDLINE_LINUX=""$GRUB_CMDLINE_LINUX="root=ZFS=rpool/ROOT/debian"$' /etc/default/grub
-
-sed -i 's$GRUB_CMDLINE_LINUX_DEFAULT="quiet"$GRUB_CMDLINE_LINUX_DEFAULT=""$' /etc/default/grub
-
-sed -i 's$#GRUB_TERMINAL=console$GRUB_TERMINAL=console$' /etc/default/grub
-
-update-grub
-
-
-grub-install --target=x86_64-efi --efi-directory=/boot/efi \
-     --bootloader-id=debian --recheck --no-floppy
-
-mkdir /etc/zfs/zfs-list.cache
-touch /etc/zfs/zfs-list.cache/bpool
-touch /etc/zfs/zfs-list.cache/rpool
-zed -F
-
-echo "Hopefully these aren't empty"
-cat /etc/zfs/zfs-list.cache/bpool
-cat /etc/zfs/zfs-list.cache/rpool
-
-
-
- 
-sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/*
-
-zfs snapshot bpool/BOOT/debian@install
-zfs snapshot rpool/ROOT/debian@install
-
-exit
 
 mount | grep -v zfs | tac | awk '/\/mnt/ {print $3}' | \
     xargs -i{} umount -lf {}
