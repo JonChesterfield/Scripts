3dd9363f2a5dbec76fc130692f7839c7479f564b
jon <jonathanchesterfield@gmail.com>
2025-12-12 19:10:14 +0000

    deduplicating scripts vs emacs.d

--- util/rebase_on_main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-BRANCH=$(git branch --show-current) && git checkout main && git pull && git checkout "$BRANCH" && git rebase main
--- util/update_main.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-REV=$(git rev-parse HEAD) && git reset --hard HEAD~1 && git pull && git cherry-pick $REV
